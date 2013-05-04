5c1568c2bd62256262668060dc6cc22817a01b3e
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-05-04 22:58:03 +0200

    Skip install default true for generator tests

--- app/templates/test-creation.js
+++ app/templates/test-creation.js
@@ -29,7 +29,7 @@ describe('<%= generatorName %> generator', function () {
     helpers.mockPrompt(this.app, {
       'someOption': 'Y'
     });
-
+    this.app.options['skip-install'] = true;
     this.app.run({}, function () {
       helpers.assertFiles(expected);
       done();
