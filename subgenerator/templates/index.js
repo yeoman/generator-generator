be0e252ec0b435513aaabf095ab543fcc629f6fb
Olivier Louvignes <olivier@mg-crea.com>
2013-06-24 16:34:42 +0200

    Add support for hyphens in generator names

--- subgenerator/templates/index.js
+++ subgenerator/templates/index.js
@@ -2,7 +2,7 @@
 var util = require('util');
 var yeoman = require('yeoman-generator');
 
-var <%= _.capitalize(generatorName) %>Generator = module.exports = function <%= _.capitalize(generatorName) %>Generator(args, options, config) {
+var <%= _.classify(generatorName) %>Generator = module.exports = function <%= _.classify(generatorName) %>Generator(args, options, config) {
   // By calling `NamedBase` here, we get the argument to the subgenerator call
   // as `this.name`.
   yeoman.generators.NamedBase.apply(this, arguments);
@@ -10,8 +10,8 @@ var <%= _.capitalize(generatorName) %>Generator = module.exports = function <%= 
   console.log('You called the <%= generatorName %> subgenerator with the argument ' + this.name + '.');
 };
 
-util.inherits(<%= _.capitalize(generatorName) %>Generator, yeoman.generators.NamedBase);
+util.inherits(<%= _.classify(generatorName) %>Generator, yeoman.generators.NamedBase);
 
-<%= _.capitalize(generatorName) %>Generator.prototype.files = function files() {
+<%= _.classify(generatorName) %>Generator.prototype.files = function files() {
   this.copy('somefile.js', 'somefile.js');
 };
