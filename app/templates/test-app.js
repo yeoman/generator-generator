0ddfd7695c6ce9fb0bdef8a16bb7c9b67d475519
Simon Boudrias <admin@simonboudrias.com>
2014-08-17 23:29:37 -0700

    Run generated test in the system temp dir

--- app/templates/test-app.js
+++ app/templates/test-app.js
@@ -4,11 +4,12 @@
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
+var os = require('os');
 
 describe('<%= generatorName %>:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(__dirname, './temp'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
         someOption: true
