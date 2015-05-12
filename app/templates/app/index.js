e6d61ac0e04c9394e701c90715abbfb1f07ffe52
Simon Boudrias <admin@simonboudrias.com>
2015-05-12 13:29:31 -0700

    Remove this.pkg - not used in the generated generators

--- app/templates/app/index.js
+++ app/templates/app/index.js
@@ -4,10 +4,6 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
-
   prompting: function () {
     var done = this.async();
 
