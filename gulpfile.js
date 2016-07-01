b4cb8743f5891f00b4068667b703b5bab4fcdf25
Simon Boudrias <admin@simonboudrias.com>
2016-07-01 11:18:58 -0700

    Augment test timeout

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('test', ['pre-test'], function (cb) {
 
   gulp.src('test/**/*.js')
     .pipe(plumber())
-    .pipe(mocha({reporter: 'spec', timeout: 5000}))
+    .pipe(mocha({reporter: 'spec', timeout: 10000}))
     .on('error', function (err) {
       mochaErr = err;
     })
