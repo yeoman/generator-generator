559cfadb3b7e94aba8b8728aee71dc89b06713ff
Michael Kühnel <m.kuehnel@micromata.de>
2018-02-25 21:22:46 +0100

    Fix additional linting errors in generated generator
    Needed to set rule object-curly-spacing to `always` because otherwise it would conflict to the prettier setup in the generated generator.

--- subgenerator/templates/test.js
+++ subgenerator/templates/test.js
@@ -5,13 +5,12 @@ const helpers = require('yeoman-test');
 
 describe('<%- generatorName %>:<%- name %>', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/<%- name %>'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/<%- name %>'))
+      .withPrompts({ someAnswer: true });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['dummyfile.txt']);
   });
 });
