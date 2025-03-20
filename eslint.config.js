4860852cf1ce6c03f039f31eb8a250fda5bb49f6
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-20 09:38:16 -0400

    I have done all I am willing to do to work with Jest

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
